bbfdb50f4e320eaaee34c47c031be8b28f44ac3e
vagrant <vagrant@precise64.(none)>
2015-02-13 14:50:39 +0000

    modified bootstrap to use /dev/null on output

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-apt-get update
+echo "Getting update:"
+apt-get update > /dev/null
 
-apt-get install -y git
+echo "Installing git"
+apt-get install -y git > /dev/null
 # apt-get install -y php5
 # apt-get install -y apache2
 
@@ -13,6 +15,7 @@ apt-get install -y git
 
 # installs webupd8team repository to install java jdk
 
+echo "Installing webupd8team repository for java:"
 
 
 echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee /etc/apt/sources.list.d/webupd8team-java.list
@@ -22,16 +25,20 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
 # apt-get install -y oracle-java7-installer
 
 # to automate dialog with oracle installer:
-apt-get update
+echo "Updating after adding repository:"
+apt-get update > /dev/null
 
-echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-apt-get install -y oracle-java7-installer
+echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections > /dev/null
+echo "installing the java7 installer:"
+apt-get install -y oracle-java7-installer > /dev/null
 
 
 # git config --global user.name "agcilantro"
 # git config --global user.email agcilantro@gmail.com
 #wget http://downloads.sourceforge.net/project/opentaps/opentaps%201.5/opentaps-1.5.0.zip?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fopentaps%2Ffiles%2Fopentaps%25201.4%2F&ts=1423709025&use_mirror=colocrossing opentaps
- git clone --recursive https://gitorious.org/opentaps/agcilantros-integratingweb-opentaps.git opentaps
+echo "change to vagrant folder and clone opentaps from git:"
+cd /vagrant 
+ git clone --recursive --verbose https://gitorious.org/opentaps/agcilantros-integratingweb-opentaps.git opentaps
 
 # change ownership of opentaps folder to vagrant:opentaps, chmod opentaps folder to 700
 sudo addgroup opentaps
@@ -57,4 +64,4 @@ sudo chmod 700 /vagrant/opentaps
 
 
 
-fi
+
